1b89168b0c19cb538e5a159e5397d005638aab75
Piotr Solar <bgrhg@wp.pl>
2020-03-26 21:41:26 +0100

    zmaian

--- PAI/1_struktura.php
+++ PAI/1_struktura.php
@@ -1,13 +1,70 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Zadanie</title>
+    <style>
+        table, td {
+            border: 1.5px solid black;
+            border-collapse: collapse;
+            width: 50%;
+            height: 50%;
+            text-align: center;
+        }
+        th{
+            border:0px;
+            border-collapse: collapse;
+            height:20px;
+            padding:0px;
+          
+        }
+ 
+</style>
 </head>
 <body>
-    <?php
-    echo "soalr ma ma³êgo pindola";
-    ?>
+    
+        <div>
+            <h1 style="font-size:2vw">Zadanie</h1>
+        </div>
+        
+        
+        <div>
+                    <h3 style="font-size:1vw">Lorem Ipsum jest tekstem stosowanym jako przyk³adowy wype³niacz w przemyœle poligraficznym. Zosta³ po raz pierwszy u¿yty w XV w. przez nieznanego drukarza do wype³nienia tekstem próbnej ksi¹¿ki. Piêæ wieków póŸniej zacz¹³ byæ u¿ywany przemyœle elektronicznym, pozostaj¹c praktycznie niezmienionym. Spopularyzowa³ siê w latach 60. XX w. wraz z publikacj¹ arkuszy Letrasetu, zawieraj¹cych fragmenty Lorem Ipsum, a ostatnio z zawieraj¹cym ró¿ne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker</h3>
+        </div>
+        
+        
+        <div>
+            <img src="e2.png" style="width:50%;">
+        </div>
+       
+        
+        <div>
+      
+<table>
+  <tr>
+    <td rowspan="2">1.1</td>
+    <td >1.2</td>
+  </tr>
+  <tr>
+    <td rowspan="2">2.2</td>
+      <th>
+      </th>
+      
+  </tr>
+  <tr>
+    <td>31.</td>
+  </tr>
+  <tr>
+    <td colspan="2">4.1</td>
+  </tr>
+</table>
+        
+        </div>
+        <div>
+        <h2 style="font-size:0.8vw">"Mam ma³êgo pindola "~~ Piotr Solarski</h2>
+        </div>
+    
 </body>
 </html>
